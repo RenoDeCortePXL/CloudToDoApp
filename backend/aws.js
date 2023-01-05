597930dd75b657b1f2a66e12e0946f89526c6f55
RenoDeCortePXL <73355392+RenoDeCortePXL@users.noreply.github.com>
2023-01-05 16:10:03 +0100

    Update aws.js

--- backend/aws.js
+++ backend/aws.js
@@ -1,11 +1,11 @@
 const AWS = require('aws-sdk');
 
-/*
+
 # These settings can be used if no propper IAM roles are set up or when running locally
 AWS.config.update({accessKeyId: process.env.AWS_ACCESS_KEY_ID});
 AWS.config.update({secretAccessKey: process.env.AWS_ACCESS_KEY});
 AWS.config.update({region: process.env.AWS_REGION});
-*/
+AWS.config.update({sessionToken: process.env.AWS_SESSION_TOKEN});
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 
